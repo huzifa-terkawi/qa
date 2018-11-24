2763f86593d84409021e4cfd7036e05bf8c0d44c
unknown <huzifa30@gmail.com>
2018-11-24 06:26:20 +0200

    build question form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,35 +5,43 @@
         <div class="row justify-content-center">
             <div class="col-md-11">
                 <div class="card">
-                    <div class="card-header">All Questions</div>
-
-                    <div class="card-body">
-                       @foreach($questions as $q)
-                        <div class="media">
-                            <div class="d-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{$q->votes}}</strong> {{ str_plural('vote',$q->votes) }}
-                                </div>
-                                <div class="status {{$q->status}}">
-                                    <strong>{{$q->answers}}</strong> {{ str_plural('answer',$q->answers) }}
-                                </div>
-                                <div class="view">
-                                    {{$q->views . " " . str_plural('view',$q->views) }}
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $q->url }}">{{$q->title}}</a></h3>
-                                <p class="lead">
-                                    Asked By <a href="{{$q->user->url}}">{{$q->user->name}}</a>
-                                    <small class="text-muted">{{$q->created_date}}</small>
-                                </p>
-                                {{ str_limit($q->body,250) }}
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>All Questions</h2>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.create')}}" class="btn btn-outline-secondary">Ask
+                                    Question</a>
                             </div>
                         </div>
+                    </div>
+
+                    <div class="card-body">
+                        @foreach($questions as $q)
+                            <div class="media">
+                                <div class="d-flex flex-column counters">
+                                    <div class="vote">
+                                        <strong>{{$q->votes}}</strong> {{ str_plural('vote',$q->votes) }}
+                                    </div>
+                                    <div class="status {{$q->status}}">
+                                        <strong>{{$q->answers}}</strong> {{ str_plural('answer',$q->answers) }}
+                                    </div>
+                                    <div class="view">
+                                        {{$q->views . " " . str_plural('view',$q->views) }}
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <h3 class="mt-0"><a href="{{ $q->url }}">{{$q->title}}</a></h3>
+                                    <p class="lead">
+                                        Asked By <a href="{{$q->user->url}}">{{$q->user->name}}</a>
+                                        <small class="text-muted">{{$q->created_date}}</small>
+                                    </p>
+                                    {{ str_limit($q->body,250) }}
+                                </div>
+                            </div>
                             <hr>
                         @endforeach
                         <div class="text-center">
-                        {{$questions->links()}}
+                            {{$questions->links()}}
                         </div>
                     </div>
                 </div>
